fa18a2fdb0313c31865c833359453f893583874e
Tuluobo <tuluobo@vip.qq.com>
2018-06-30 14:32:47 +0800

    Initial Commit

---
+++ Leiter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Leiter
+//
+//  Created by Hao Wang on 2018/6/30.
+//  Copyright © 2018 Tuluobo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
